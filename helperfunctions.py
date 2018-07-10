8aa7494376717f8901583e210428aeea35631ce0
kw68021 <40372828+kw68021@users.noreply.github.com>
2018-07-10 13:56:38 -0700

    Add files via upload

---
+++ helperfunctions.py
@@ -0,0 +1,16 @@
+
+def add(x,y):
+    return x + y
+
+def diff(x,y):
+    return x - y
+
+def product(x,y):
+    return x*y
+
+def greatest(x,y):
+    if x > y:
+        return x
+    else:
+        return y
+### END CODE
